09468e5ffe3c980760830854b1b95544257b78eb
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-16 08:10:06 -0500

    feat: render header repos

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,21 +1,29 @@
 import { githubApiResponses } from "../../github_api_responses";
-import { ReactComponent as Lock } from "./lock.svg";
-import { ReactComponent as Unlock } from "./unlock.svg";
+
+import styles from "./Dashboard.module.scss";
+import brand from "./brand.svg";
+import unlock from "./unlock.svg";
+import lock from "./lock.svg";
 
 export function Dashboard() {
   return (
     <>
-      <header>
-        <section>
+      <header className={styles.header}>
+        <section className={styles.header_container}>
+          <img src={brand} alt="" />
           <h1>DevDash</h1>
         </section>
       </header>
-      <section>
+      <section className={styles.container}>
         {githubApiResponses.map((widget) => (
-          <article>
+          <article className={styles.widget} key={widget.repositoryData.id}>
             <header>
-              <a href="">
-                {widget.repositoryData.private ? <Lock /> : <Unlock />}
+              <a href={widget.repositoryData.html_url}>
+                {widget.repositoryData.private ? (
+                  <img alt="icon" src={lock} />
+                ) : (
+                  <img alt="icon" src={unlock} />
+                )}
               </a>
             </header>
           </article>
